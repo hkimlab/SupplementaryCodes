e85507464d2b3fff93a51aab0bc0084180702d65
hkimlab <44887743+hkimlab@users.noreply.github.com>
2018-11-09 16:48:58 +0900

    Supplemental Code 3 - t0 and HL Calculation

--- Supplementary Code 3 - t0 and HL Calculation/functions/calc_AIC.R
+++
@@ -1,5 +0,0 @@
-calc_AIC <- function(MSE, n, p){
-  
-  n + n * log(2 * pi) + n * log(MSE) + 2 * p
-  
-}
--- Supplementary Code 3 - t0 and HL Calculation/functions/calc_BIC.R
+++
@@ -1,5 +0,0 @@
-calc_BIC <- function(MSE, n, p){
-  
-  n + n * log(2 * pi) + n * log(MSE) + log(n) * p
-  
-}
